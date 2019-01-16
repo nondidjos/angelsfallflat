ee3077159457619bf0e1e4af616ce8ef74700cfd
nondidjos <46749428+nondidjos@users.noreply.github.com>
2019-01-16 21:25:37 +0100

    added comments and fixed rotation

--- AISN_Firefly.lua
+++ AISN_Firefly.lua
@@ -2,6 +2,8 @@ local AISN_Firefly = {}
 
 --AISN_Firefly.health = 50
 
+--setting up a new 'table', the switch between left and the right sprite and giving the thing a width and height (see main.lua for the values) and centering on the screen dividing the resolution by 2
+--also setting up the body for our firefly 'object'
 function AISN_Firefly.new(self, l, r, w, h)
     o = {}
     self.imgl = l
@@ -13,7 +15,7 @@ function AISN_Firefly.new(self, l, r, w, h)
     self.cy = self.h/2
 -- coordonates of the center after rotation
     self.tr_x = self.cx
-    self.tr_y = self.cx
+    self.tr_y = self.cy
     AISN_Firefly.screen_position = {}
     AISN_Firefly.screen_position.x = playerX/2 - self.cx
     AISN_Firefly.screen_position.y = playerY/2 - self.cy
@@ -37,6 +39,7 @@ function AISN_Firefly.new(self, l, r, w, h)
     return o
 end
 
+--accelerating our object via the existing velocity values
 function AISN_Firefly.accelerate(self, accelx, accely)
     if accelx ~= nil then
         self.velocity.x = self.velocity.x + accelx
@@ -46,23 +49,29 @@ function AISN_Firefly.accelerate(self, accelx, accely)
     end
 end
 
+--trying to center the rotation on the center of our ship
 function AISN_Firefly.translate_rot(cx, cy, rot)
+    orig_rot = math.atan2(cy, cx)
     r = math.sqrt(cx^2 + cy^2)
-    x = r*math.cos(rot)
-    y = r*math.sin(rot)
+    x = r*math.cos(rot + orig_rot)
+    y = r*math.sin(rot + orig_rot)
     return x, y
 end
 
+--here we draw the ship and we try to recenter the ship substracting the translate_rot value
 function AISN_Firefly.draw(self)
     
     love.graphics.draw(myAISN_Firefly.img, AISN_Firefly.screen_position.x - self.tr_x, AISN_Firefly.screen_position.y - self.tr_y, AISN_Firefly.r)
 end
 
+--here we make our own update callback, we calculate how fast our ship is going and the rotation with some wisardry
+--the we set the position of our body with the screenpositions and adding in the offset
+--and finally we determine when to flip the sprites so it always looks upright and we return all of this for main.lua to require
 function AISN_Firefly.updateposition(self, dt)
     self.world_position.x = self.world_position.x + dt * self.velocity.x
     self.world_position.y = self.world_position.y + dt * self.velocity.y
 
-    self.r = calc_rot(self.body:getX(), self.body:getY()) + math.pi
+    self.r = calc_rot(self.screen_position.x, self.screen_position.y) + math.pi
     self.body:setAngle(self.r)
 
     self.tr_x, self.tr_y = self.translate_rot(self.cx, self.cy, self.r)
--- main.lua
+++ main.lua
@@ -1,13 +1,15 @@
--- version 0.04
+-- version 0.05
 -- by nondidjos
 
 
+--we setup the world, get the monitor size and then require all the code we did in AISN_Firefly
 world = love.physics.newWorld(0, 0, true)
 
 playerX, playerY = love.window.getDesktopDimensions(desktop)
 
 AISN_Firefly = require("AISN_Firefly")
 
+--here we load the images we need and set the width and height of our object and the things we only need to execute once
 function love.load()
     myAISN_Firefly = AISN_Firefly:new(love.graphics.newImage("AISN_Ti_l.png"), love.graphics.newImage("AISN_Ti_r.png"), 186, 78)
 --    myAISN_Salamender = AISN_Salamender:new(love.graphics.newImage())
@@ -15,6 +17,7 @@ function love.load()
     love.window.setFullscreen(true , "desktop")
 end
 
+--we move the world here depending on the screen postions 
 function love.draw()
     PIXION_x = myAISN_Firefly.screen_position.x - myAISN_Firefly.world_position.x
     PIXION_y = myAISN_Firefly.screen_position.y - myAISN_Firefly.world_position.y
@@ -22,8 +25,11 @@ function love.draw()
     love.graphics.draw(PIXION, PIXION_x, PIXION_y)
 
     myAISN_Firefly:draw()
+--    love.graphics.rectangle("fill", myAISN_Firefly.screen_position.x, myAISN_Firefly.screen_position.y, 186, 76)
+
 end
 
+--doing the math to rotate the ship returning the rot value
 function calc_rot(x, y)
     curX, curY = love.mouse.getPosition()
     DX = curX - x
@@ -32,6 +38,8 @@ function calc_rot(x, y)
     return rot
 end
 
+--here we update the world so that the body's position gets movin' and we check for keypresses, applying acceleration to our ship
+--then we run the update callback on AISN_Firely
 function love.update(dt)
     world:update(dt)
 --    AISN_Firefly:getLinearDampning()
